3e709a0568bb189a07e39d690dd429fd5b2142a1
yukihanawa <hanawayuki@hanawayukinoMacBook-Air.local>
2023-10-01 17:57:15 +0900

    メインループの中身を理解し、コメントを追加

--- my_IB.m
+++ my_IB.m
@@ -40,6 +40,71 @@ while FE < maxFE
     ssr = randperm(pop_size); %1からpop_sizeまでの数字をランダムに並べたベクトルを作成
     parent = pop(:,ssr); %個体の座標を並べ直す
     parentfit = fit(:,ssr); %個体の評価値を並べ直す
+    
+    %交叉に使う親個体を選択
+    spc = ssr(1:nc);
+    %突然変異させる親個体を選択（交叉しなかった親全部）
+    spm = ssr(nc+1: end);
+    
+    %交叉
+    parentc = pop(:, spc);
+    offspringc = zeros(size(parentc));
+    for k=1:nc/2
+        p1 = parentc(:, 2*k-1);
+        p2 = parentc(:, 2*k);
+        
+        [offspringc(:, 2*k-1),offspringc(:, 2*k)] = Crossover(p1, p2, gamma,ub(1),lb(1));
+    end
+    
+    %突然変異
+    parentm = pop(:, spm);
+    offspringm = zeros(size(parentm));
+    for k = 1: nm
+        p = parentm(:, k);
+        offspringm(:,k) = Mutate(p, mu, ub(1), lb(1));
+    end
+    
+    %新たな子個体
+    offspring = [offspringc offspringm];
+
+    % 再評価の割合
+    psm = floor(rsm * pop_size);  % 実評価関数によって評価される個体数
+
+    % 評価値を予測（実評価関数を使う（ノイズあり））
+    offspring_fit_assumed = eval_pop(fhd_noise, offspring);
+    fe = fe + pop_size;  % 評価回数を更新
+    
+    % 予測値を元に並び替え
+    [sorted_fit, index] = sort(offspring_fit_assumed);
+
+    % 再評価する個体を選ぶ
+    reevaluate_pop = offspring(:, index(1:psm));
+    reevaluate_fit = sorted_fit(1:psm);
+
+    % 本当はここでアーカイブを残す（座標、評価値→学習のため）
+    %今回はサロゲートを学習しないので、省略
+    
+    %最良個体を残す
+    [bestfit, index] = min(parentfit);
+    bestind = parent(:, index);
+    %最良個体を取り除く（後で付け足す）
+    parent(:, index) = [];
+    parentfit(index) = [];
+
+    % 次世代に残る候補を集める（親、再評価された子個体、再評価されなかった子個体）
+    pop = [parent reevaluate_pop  offspring(:, index(psm+1:end))];
+    fit = [parentfit reevaluate_fit sorted_fit(index(psm+1:end))];
+
+    % 候補の評価値を元に並び替え
+    [fit, index] = sort(fit);
+    pop = pop(:, index);
+
+    % 最良個体を追加している
+    pop = [bestind pop(:, 1:pop_size-1)];
+    fit = [bestfit fit(:, 1:pop_size-1)];
+
+    fprintf('FE: %d, Fitness: %.2e \n', fe, min(arcv.y));
+
 end  
 
 % Helper functions (You need to fill these up)
