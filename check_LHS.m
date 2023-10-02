91c912e1d97322fd6e278c9eb09f1dcc2dfc7000
yukihanawa <hanawayuki@hanawayukinoMacBook-Air.local>
2023-10-03 04:16:35 +0900

    IB_absoluteが一応動くようにはなった

--- check_LHS.m
+++ check_LHS.m
@@ -1,7 +1,5 @@
-dim = 2;
-ub = 10 * ones(dim,1)
-lb = -10 * ones(dim,1) 
+fit = [5,3,4,2,3,1];
 
-a = repmat(lb', 5*dim,1)
-b = lhsdesign(5*dim,dim, 'iterations',1000)
-c = b.*(repmat(ub' - lb', 5*dim, 1))
+fit = sort(fit);
+
+fit
