9bf575da6132f6a5e1c580d46cc94dc258d98842
yukihanawa <hanawayuki@hanawayukinoMacBook-Air.local>
2023-10-24 17:53:40 +0900

    ibrbfを追加

--- compare.m
+++ compare.m
@@ -1,9 +1,9 @@
 clear;
-dim = 30;
+dim = 10;
 knn = [1*dim, 2*dim, 3*dim, 4*dim, 5*dim];
 rsm = [0.1 0.2 0.5 0.7 0.9];
 
-for run = 1:1:10
+for run = 11:1:20
     seed = run*100 + 2019;
     fprintf('NoS is starting....')
     for func = [1 2 4 8 13 15]
@@ -27,6 +27,20 @@ for run = 1:1:10
             save(filename, 'evolve')
             filename = sprintf('pssvc_csv/pssvc_run%d_f%d_d%d_sp%.2f.csv',run,func,dim,sp);
             filled_data = fillmissing(min_hist(1:end),'previous');
+            csvwrite(filename, filled_data)
+        end
+    end
+    fprintf('\n\n\n')
+    seed = run * 100 + 2019;
+    fprintf('IBRBF is starting....')
+    for func = [1 2 4 8 13 15]
+        for sp = [0.5 0.6 0.7 0.8 0.9 1.0]
+            fprintf('func: %d', func);    
+            [evolve, min_hist] = IBRBF( func, dim, seed, sp);
+            filename = sprintf('IBRBF/ibrbf_run%d_f%d_d%d_sp%.2f.mat', run, func, dim,sp);
+            save(filename, 'evolve')
+            filename = sprintf('ibrbf_csv/ibrbf_run%d_f%d_d%d_sp%.2f.csv',run,func,dim,sp);
+            filled_data = fillmissing(min_hist(1:2000),'previous');
             csvwrite(filename, filled_data)
         end
     end
