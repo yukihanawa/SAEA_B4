c1ff49e3b8ccef4a259830602bbeaaa644480098
yukihanawa <hanawayuki@hanawayukinoMacBook-Air.local>
2023-10-24 16:29:02 +0900

    IBRBFに変更

--- IBRBF.m
+++ IBRBF.m
@@ -1,136 +1,131 @@
 function arcv = IBRBF( func_num, dim, seed, knn, rsm)
+% Parameter settings
 
 
-rng(seed);
-lb = -100 * ones(dim, 1);
-ub = 100 * ones(dim, 1);
+psr = 0.5;%pre-selection rate
+
+rng(seed); %seed値を設定する
+lb = -100 * ones(dim,1);%lower bound
+ub = 100 * ones(dim,1);%upper bound
+
+maxFE = 200*dim; % maximum function evaluations
+pop_size = 40; % number of population
+
+rsm = 0.5;
+
+% parameter
+pc = 0.7; %交叉率
+nc = 2 * round(pc * pop_size/2); %子孫の数
+gamma = 0.4; %extra range factor for crossover
+
+pm = 0.3; %突然変異率
+nm = round(pm*pop_size); %突然変異数
+mu = 0.3;
+
+sp = 0.7;%予測成功確率
+
 fhd = @(x)(cec15problems('eval',x,func_num));
 
-
-maxfe = 200 *dim;
-pop_size =40;
-
-% LHS database initilization
-arcv.x = repmat(lb', 5*dim, 1) + lhsdesign(5*dim, dim, 'iterations', 1000) .* (repmat(ub' - lb', 5*dim, 1));
+%LHS(Latin Hypercube Sampling)を使ってサンプルを生成
+arcv.x = repmat(lb', 5*dim,1) + lhsdesign(5*dim,dim, 'iterations',1000).*(repmat(ub' - lb', 5*dim, 1));
+%サンプルを評価
 arcv.y = eval_pop(fhd, arcv.x')';
+%ソート
+[arcv.y, index] = sort(arcv.y); % 昇順でソート
+arcv.x = arcv.x(index,:); % ソートされたインデックスを使用して、arcv.x(index,:)をソート
+%初期集団の選択
+fit = arcv.y(1:pop_size)';% 評価値
+pop = arcv.x(1:pop_size,:)';% 集団
 
-[arcv.y, index] = sort(arcv.y);
-arcv.x = arcv.x(index, :);
-fit = arcv.y(1:pop_size)';
-pop = arcv.x(1:pop_size, :)';
-fe = 5*dim;
-
-% hyperparameter for GA
-pc=0.7;                                  % crossover percentage
-nc=2*round(pc*pop_size/2);      % number of offsprings (also Parnets)
-gamma=0.4;                          % extra range factor for crossover
-
-pm=0.3;                                 % mutation percentage
-nm=round(pm*pop_size);           % number of mutants
-mu=0.3;                                  % mutation rate
+FE = 5*dim; %評価回数を更新
 
 
-% sort population
-[fit, index] = sort(fit);
-pop = pop(:, index);
+%評価値の良い順番に並び替え
+[fit, index] = sort(fit); 
+pop = pop(:,index);
 
-fprintf('FE: %d, Fitness: %.2e \n', fe, min(fit))
-
-% knn = 4 * dim;  %training size for each surrogate model 
-
-% main loop
-while fe < maxfe 
+% Main loop（最大評価回数を超える前繰り返す）
+while FE < maxFE
+    ssr = randperm(pop_size); %1からpop_sizeまでの数字をランダムに並べたベクトルを作成
+    parent = pop(:,ssr); %個体の座標を並べ直す
+    parentfit = fit(:,ssr); %個体の評価値を並べ直す
     
-    ssr = randperm(pop_size);
-    parent = pop(:, ssr);
-    parentfit = fit(:, ssr);
-    
-    % select parents for crossover
-    spc = ssr(1: nc);
-    % select parents for mutation
+    %交叉に使う親個体を選択
+    spc = ssr(1:nc);
+    %突然変異させる親個体を選択（交叉しなかった親全部）
     spm = ssr(nc+1: end);
     
-    % Crossover
+    %交叉
     parentc = pop(:, spc);
     offspringc = zeros(size(parentc));
     for k=1:nc/2
+        p1 = parentc(:, 2*k-1);
+        p2 = parentc(:, 2*k);
         
-        p1=parentc(:, 2*k-1);
-        p2=parentc(:, 2*k);
-        
-        [offspringc(:, 2*k-1), offspringc(:, 2*k)]=Crossover(p1, p2, gamma, ub(1), lb(1));
-        
+        [offspringc(:, 2*k-1),offspringc(:, 2*k)] = Crossover(p1, p2, gamma,ub(1),lb(1));
     end
     
-    
-    % Mutation
+    %突然変異
     parentm = pop(:, spm);
     offspringm = zeros(size(parentm));
-    for k=1:nm 
-        
-        p=parentm(:, k);
-        
-        offspringm(:, k)=Mutate(p,mu,ub(1), lb(1));
-        
+    for k = 1: nm
+        p = parentm(:, k);
+        offspringm(:,k) = Mutate(p, mu, ub(1), lb(1));
     end
     
-    % new offspring
+    %新たな子個体（交叉したものと突然変異させたものを組み合わせる）
     offspring = [offspringc offspringm];
+
+    % 再評価の割合
+    psm = floor(rsm * pop_size);  % 実評価関数によって評価される個体数
     
-    % Model management:  The ¦Ë highest ranked individuals  are
-    % evaluated by the original model. The remaining are predicted 
-    % by surrogate model 
+
+    % 評価値を予測（実評価関数を使う（ノイズあり））
+    offspring_fit_assumed = eval_pop(fhd, offspring);
     
-    % ++++ ratio of re-evaluation++++
-    psm = floor(rsm* pop_size); 
     
-    % ------------------------------------------
-    % -------------- RBF model--------------
-    % ------------------------------------------
-    
-    offspring_predict_y = zeros(1, pop_size);
-    for i=1:pop_size
-        train = get_traing_data(arcv, knn, offspring(:, i)');
-        srgtOPTKRG  = srgtsRBFSetOptions(train.x, train.y);
-        RBFModel = srgtsRBFFit(srgtOPTKRG);
-        offspring_predict_y(i) = srgtsRBFEvaluate(offspring(:, i)', RBFModel);
-    end
-    [~, index] = sort(offspring_predict_y);
+    % 予測値を元に並び替え
+    [sorted_fit, index] = sort(offspring_fit_assumed);
+
+    % 再評価する個体を選ぶ（評価値が良いとされていたもの）
     reevaluate_pop = offspring(:, index(1:psm));
+    %再評価
     reevaluate_fit = eval_pop(fhd, reevaluate_pop);
-    fe = fe + psm;
+    FE = FE + psm;  % 評価回数を更新
+
+    % 本当はここでアーカイブを残す（座標、評価値→学習のため）
+    %今回はサロゲートを学習しないので、省略
     
-    % save the re-evaluated individuals
-    arcv.x = [arcv.x; reevaluate_pop'];
-    arcv.y = [arcv.y; reevaluate_fit'];
-    
-     %reserve the best individual in parent population
+    %最良個体を残す
     [bestfit, index] = min(parentfit);
     bestind = parent(:, index);
+    %最良個体を取り除く（後で付け足す）
     parent(:, index) = [];
     parentfit(index) = [];
-    
-    % select new parents
-    pop = [parent reevaluate_pop  offspring(:, index(psm+1: end))];
-    fit = [parentfit reevaluate_fit offspring_predict_y(index(psm+1: end))];
-    
-    
-    % select new parents
+
+    % 次世代に残る候補を集める（親、再評価された子個体、再評価されなかった子個体）
+    pop = [parent reevaluate_pop  offspring(:, index(psm+1:end))];
+    fit = [parentfit reevaluate_fit offspring_fit_assumed(index(psm+1:end))];
+
+    % 候補の評価値を元に並び替え
     [fit, index] = sort(fit);
     pop = pop(:, index);
-    
-    % truncation
+
+    % 最良個体を追加している
     pop = [bestind pop(:, 1:pop_size-1)];
-    fit = [bestfit fit(:, 1:pop_size)];
-    fprintf('FE: %d, Fitness: %.2e \n', fe, min(arcv.y));
-     
+    fit = [bestfit fit(:, 1:pop_size-1)];
+
+    %fprintf('FE: %d, Fitness: %.2e \n', FE, min(fit));
+    fprintf('%.2e\n',min(fit));
+
+end  
 end
 
-end
 
+%評価値を計算
 function fit = eval_pop(f, pop)
     
-    [dim, n] = size(pop);
+    [~ , n] = size(pop);
     fit = zeros(1, n);
     for i = 1:n
         fit(i) = f(pop(:, i));
@@ -138,16 +133,8 @@ function fit = eval_pop(f, pop)
 
 end
 
-function train = get_traing_data(arx, k, ind)
 
-    dis = pdist2(ind, arx.x)';
-    [~, index] = sort(dis);
-    train.x = arx.x(index(1:k), :);
-    train.y = arx.y(index(1:k));
-
-end
-
-% crossover operator
+% 交叉関数
 function [y1, y2]=Crossover(x1,x2,gamma,VarMax,VarMin)
 
     alpha=unifrnd(-gamma,1+gamma,size(x1));
@@ -163,11 +150,11 @@ function [y1, y2]=Crossover(x1,x2,gamma,VarMax,VarMin)
 
 end
 
-% mutation operator
+% 突然変異関数
 function y=Mutate(x,mu,VarMax,VarMin)
     
     nVar=size(x,1);
-    nmu=ceil(mu*nVar);
+    %nmu=ceil(mu*nVar);
     
    % original mutation     
 %     j=randsample(nVar,nmu)';
