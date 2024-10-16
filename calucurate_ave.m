de28f31b340aebbe7bb4f33b95037acadec490ed
yuki hanawa <yh.20.hp.731@gmail.com>
2024-10-16 15:13:14 +0900

    calucurate mean with 21 runs

--- calucurate_ave.m
+++ calucurate_ave.m
@@ -1,42 +1,42 @@
 % 設定
 f_value = [1,2,4,8,13,15]; % fの値
-d_value = 10; % dの値
+d_value = 30; % dの値
 num_runs = 21; % runの数
 sp_value = [0.5, 0.6, 0.7, 0.8, 0.9, 1.0];
 
 % 対象となるファイル名のプレフィックスを配列に格納
-file_prefixes = {'gbafs'};
+file_prefixes = {'ibafs','gbafs','pssvc'};
 % nosresult用
 
-% prefix = 'nosresult';
-% for f = 1:length(f_value)
-%     f_v = f_value(f);
-%     % 2000x10の行列を初期化（各CSVファイルのデータがここに格納されます）
-%     all_data = zeros(2000, num_runs);
-% 
-%     % 各CSVファイルからデータを読み込む
-%     % 各CSVファイルからデータを読み込む
-% 
-%     for run = 1:num_runs
-%         filename = sprintf('%s_csv/%s_run%d_f%d_d%d.csv',prefix, prefix, run, f_v, d_value);
-%         if ~exist(filename, 'file')
-%             fprintf('ファイルが存在しません: %s\n', filename);
-%             continue; % 次のループへ進む
-%         end
-%         try
-%             all_data(:, run) = csvread(filename);
-%         catch ME
-%             fprintf('エラーが発生しました: %s\n', ME.message);
-%         end
-%     end
-% 
-% 
-% %     % 平均値を計算
-% %     mean_values = mean(all_data, 2);
-% % 
-% %     % 平均値を最後の列に追加
-% %     all_data_with_mean = [all_data, mean_values];
-% 
+prefix = 'nosresult';
+for f = 1:length(f_value)
+    f_v = f_value(f);
+    % 2000x10の行列を初期化（各CSVファイルのデータがここに格納されます）
+    all_data = zeros(2000, num_runs);
+
+    % 各CSVファイルからデータを読み込む
+    % 各CSVファイルからデータを読み込む
+
+    for run = 1:num_runs
+        filename = sprintf('%s_csv/%s_run%d_f%d_d%d.csv',prefix, prefix, run, f_v, d_value);
+        if ~exist(filename, 'file')
+            fprintf('ファイルが存在しません: %s\n', filename);
+            continue; % 次のループへ進む
+        end
+        try
+            all_data(:, run) = csvread(filename);
+        catch ME
+            fprintf('エラーが発生しました: %s\n', ME.message);
+        end
+    end
+
+
+    % 平均値を計算
+    mean_values = mean(all_data, 2);
+
+    % 平均値を最後の列に追加
+    all_data_with_mean = [all_data, mean_values];
+
 %     %中央値ver
 %     % 最終行のデータから中央値を計算
 %     final_row = all_data(end, :);
@@ -50,41 +50,41 @@ file_prefixes = {'gbafs'};
 % 
 %     % 中央値の列を追加
 %     all_data_with_mean = [all_data, median_column];
-% 
-%     % 新しいCSVファイルに保存
-%     new_filename = sprintf('combine_results/aggregated_%s_f%d_d%d.csv', prefix, f_v, d_value);
-%     csvwrite(new_filename, all_data_with_mean);
-% end
-% 
-% prefix = 'nos_pssvc';
-% for f = 1:length(f_value)
-%     f_v = f_value(f);
-%     % 2000x10の行列を初期化（各CSVファイルのデータがここに格納されます）
-%     all_data = zeros(2000, num_runs);
-% 
-%     % 各CSVファイルからデータを読み込む
-%     % 各CSVファイルからデータを読み込む
-% 
-%     for run = 1:num_runs
-%         filename = sprintf('%s_csv/%s_run%d_f%d_d%d.csv',prefix, prefix, run, f_v, d_value);
-%         if ~exist(filename, 'file')
-%             fprintf('ファイルが存在しません: %s\n', filename);
-%             continue; % 次のループへ進む
-%         end
-%         try
-%             all_data(:, run) = csvread(filename);
-%         catch ME
-%             fprintf('エラーが発生しました: %s\n', ME.message);
-%         end
-%     end
-% 
-% 
-% %     % 平均値を計算
-% %     mean_values = mean(all_data, 2);
-% % 
-% %     % 平均値を最後の列に追加
-% %     all_data_with_mean = [all_data, mean_values];
-%     
+
+    % 新しいCSVファイルに保存
+    new_filename = sprintf('combine_results/aggregated_%s_f%d_d%d.csv', prefix, f_v, d_value);
+    csvwrite(new_filename, all_data_with_mean);
+end
+
+prefix = 'nos_pssvc';
+for f = 1:length(f_value)
+   f_v = f_value(f);
+    % 2000x10の行列を初期化（各CSVファイルのデータがここに格納されます）
+    all_data = zeros(2000, num_runs);
+
+    % 各CSVファイルからデータを読み込む
+    % 各CSVファイルからデータを読み込む
+
+    for run = 1:num_runs
+        filename = sprintf('%s_csv/%s_run%d_f%d_d%d.csv',prefix, prefix, run, f_v, d_value);
+        if ~exist(filename, 'file')
+            fprintf('ファイルが存在しません: %s\n', filename);
+            continue; % 次のループへ進む
+        end
+        try
+            all_data(:, run) = csvread(filename);
+        catch ME
+            fprintf('エラーが発生しました: %s\n', ME.message);
+        end
+    end
+
+
+    % 平均値を計算
+    mean_values = mean(all_data, 2);
+
+    % 平均値を最後の列に追加
+    all_data_with_mean = [all_data, mean_values];
+
 %      %中央値ver
 %     % 最終行のデータから中央値を計算
 %     final_row = all_data(end, :);
@@ -98,11 +98,11 @@ file_prefixes = {'gbafs'};
 % 
 %     % 中央値の列を追加
 %     all_data_with_mean = [all_data, median_column];
-% 
-%     % 新しいCSVファイルに保存
-%     new_filename = sprintf('combine_results/aggregated_%s_f%d_d%d.csv', prefix, f_v, d_value);
-%     csvwrite(new_filename, all_data_with_mean);
-% end
+
+    % 新しいCSVファイルに保存
+    new_filename = sprintf('combine_results/aggregated_%s_f%d_d%d.csv', prefix, f_v, d_value);
+    csvwrite(new_filename, all_data_with_mean);
+end
 
 
 % 各プレフィックスに対して処理
@@ -120,7 +120,7 @@ for p = 1:length(file_prefixes)
 
             for run = 1:num_runs
 %                 filename = sprintf('%s_csv/%s_run%d_f%d_d%d_sp%.2f.csv',prefix, prefix, run, f_v, d_value,sp_v);
-                filename = sprintf('%s_2_csv/%s_run%d_f%d_d%d_sp%.2f.csv',prefix , prefix, run, f_v, d_value,sp_v);
+                filename = sprintf('%s_csv/%s_run%d_f%d_d%d_sp%.2f.csv',prefix , prefix, run, f_v, d_value,sp_v);
                 if ~exist(filename, 'file')
                     fprintf('ファイルが存在しません: %s\n', filename);
                     continue; % 次のループへ進む
@@ -133,25 +133,25 @@ for p = 1:length(file_prefixes)
             end
 
             %平均値ver
-%             % 平均値を計算
-%             mean_values = mean(all_data, 2);
-% 
-%             % 平均値を最後の列に追加
-%             all_data_with_mean = [all_data, mean_values];
+            % 平均値を計算
+            mean_values = mean(all_data, 2);
+
+            % 平均値を最後の列に追加
+            all_data_with_mean = [all_data, mean_values];
             
-            %中央値ver
-            % 最終行のデータから中央値を計算
-            final_row = all_data(end, :);
-            median_value_final_row = median(final_row);
-
-            % 最終行で中央値に該当する列の値を取得
-            [~, median_column_index] = min(abs(final_row - median_value_final_row));
-
-            % 中央値を持つ列の値を全行に追加
-            median_column = all_data(:, median_column_index);
-
-            % 中央値の列を追加
-            all_data_with_mean = [all_data, median_column];
+%             %中央値ver
+%             % 最終行のデータから中央値を計算
+%             final_row = all_data(end, :);
+%             median_value_final_row = median(final_row);
+% 
+%             % 最終行で中央値に該当する列の値を取得
+%             [~, median_column_index] = min(abs(final_row - median_value_final_row));
+% 
+%             % 中央値を持つ列の値を全行に追加
+%             median_column = all_data(:, median_column_index);
+% 
+%             % 中央値の列を追加
+%             all_data_with_mean = [all_data, median_column];
 
             % 新しいCSVファイルに保存
             new_filename = sprintf('combine_results/aggregated_%s_f%d_d%d_sp%.2f.csv', prefix, f_v, d_value,sp_v);
